adedd29a382d4fc4c9a151441a44a0b01dcc9a2d
Richard Li <rzli2106@gmail.com>
2026-09-10 19:17:23 -0400

    Inital commit

---
+++ a1test.py
@@ -0,0 +1,40 @@
+"""
+Test script for module a1
+
+When run as a script, this module invokes several procedures that 
+test the various functions in the module a1.
+
+Author: Richard Li rl998 Joon LeeJL4875
+Date:   Sept 10
+"""
+import introcs
+import a1
+def testA():
+    """
+    Test procedure for Part A
+    """
+    pass
+
+def testB():
+    """
+    Test procedure for Part B
+    """
+    pass
+
+def testC():
+    """
+    Test procedure for Part C
+    """
+    pass
+
+def testD():
+    """
+    Test procedure for Part D
+    """
+    pass
+
+testA()
+testB()
+testC()
+testD()
+print('Module passed all tests.')
