cb8dd90a41805a861fc2c8b3875ec62c6815a9fb
Richard Li <rzli2106@gmail.com>
2026-09-10 19:18:39 -0400

    Inital commit

---
+++ a1app.py
@@ -0,0 +1,10 @@
+"""
+User interface for module currency
+
+When run as a script, this module prompts the user for two currencies and 
+an amount. It prints out the result of converting the first currency to 
+the second.
+
+Author: Richard Li rl998 Joon LeeJL4875
+Date:   Sept 10
+"""
