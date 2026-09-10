2d1ede020a2615bc5337fab67e6620c6c07008fe
Richard Li <rzli2106@gmail.com>
2026-09-10 19:48:21 -0400

    added test cases

--- a1test.py
+++ a1test.py
@@ -13,6 +13,33 @@ def testA():
     """
     Test procedure for Part A
     """
+    introcs.assert_equals(a1.before_space('a b'), 'a')
+    introcs.assert_equals(a1.before_space(' a'), '')
+    introcs.assert_equals(a1.before_space('a b c'), 'a')
+    introcs.assert_equals(a1.before_space('  a'), '  a')
+
+    introcs.assert_equals(a1.after_space(' a'), 'a')
+    introcs.assert_equals(a1.after_space('a '), '')
+    introcs.assert_equals(a1.after_space('  a b  '), 'a b  ')
+
+    introcs.assert_equals(a1.first_inside_quotes('"A"BCD'), 'A')
+    introcs.assert_equals(a1.first_inside_quotes('ABCD""'), '')
+    introcs.assert_equals(a1.first_inside_quotes('"abc""def"'), 'abc')
+    introcs.assert_equals(a1.first_inside_quotes('"asdf"'), 'asdf')
+
+    introcs.assert_equals(a1.get_old('{"old" "hi"}'), 'hi')
+    introcs.assert_equals(a1.get_old('{"hello" "old" "hi"}'), 'hi')
+    introcs.assert_equals(a1.get_old('{"hello" "hello" "old" "hi"}'), 'hi')
+    introcs.assert_equals(a1.get_old('{"old" "" "bye"}'), '')
+
+    introcs.assert_equals(a1.get_new('{"new" "hi"}'), 'hi')
+    introcs.assert_equals(a1.get_new('{"hello" "new" "hi"}'), 'hi')
+    introcs.assert_equals(a1.get_new('{"hello" "hello" "new" "hi"}'), 'hi')
+    introcs.assert_equals(a1.get_new('{"new" "" "bye"}'), '')
+
+    introcs.assert_equals(a1.has_error('{"Valid":true}'), False)
+    introcs.assert_equals(a1.has_error('{"Valid":false}'), True)
+
     pass
 
 def testB():
