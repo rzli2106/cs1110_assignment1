971d73d81807e94458dcce63356a1ef949983bb7
Richard Li <rzli2106@gmail.com>
2026-09-10 19:14:32 -0400

    added names date and net IDs

---
+++ a1.py
@@ -0,0 +1,10 @@
+"""
+Module for currency exchange
+
+This module provides several string parsing functions to implement a 
+simple currency exchange routine using an online currency service. 
+The primary function in this module is exchange.
+
+Author: Richard Li rl998 Joon LeeJL4875
+Date:   Sept 10
+"""
